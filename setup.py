961bd7d6615a972d2fb7a9db77cffa4de31e4912
Carl McGraw <cjmcgraw@uw.edu>
2014-12-12 18:03:16 -0800

    Provided setup.py to allow for setuptools to be utilized

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name            = "pynalysis",
+    version         = "0.1",
+    packages        = find_packages(),
+    test_suite      = "test" 
+)
